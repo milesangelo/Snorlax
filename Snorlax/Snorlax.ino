963e2b284b33367fae69e1e8225dfdc817d76330
Miles Vendetti <milesvendetti@gmail.com>
2022-08-02 21:03:14 -0600

    Adds state and defines

--- Snorlax/Snorlax.ino
+++ Snorlax/Snorlax.ino
@@ -1,15 +1,19 @@
 #include <IRremote.hpp>
-//power button = 0x45
-  //vol + =0x46
-  //vol - =0x15
-  // func/stop = 0x47
-  // rewind = 0x44
-  // play pause = 0x40
-  // forward = 0x43
-  // down = 0x07
-  // EQ = 0x19
-  // st/ rept = 0xD
-  // 0 ....9 = [0x16, 0xC, 0x18, 0x5E, 0x8, 0x1C, 0x5A, 0x42, 0x52, 0x4A]
+
+#define POWER_BUTTON 0x45
+#define VOLUME_UP 0x46
+#define VOLUME_DOWN 0x15
+#define FORWARD 0x43
+#define REWIND 0x44
+#define PLAY_PAUSE 0x40
+#define FUNC_STOP 0x47
+// still need UP
+#define DOWN 0x07
+#define EQUALIZE 0x19
+#define STOP_REPEAT 0xD
+#define ZERO 0x16
+// do the rest...
+// 0 ....9 = [0x16, 0xC, 0x18, 0x5E, 0x8, 0x1C, 0x5A, 0x42, 0x52, 0x4A]
 const int LED_PIN = 44;
 const int IR_RECEIVE_PIN = 46;
 
@@ -27,58 +31,56 @@ void setup() {
   Serial.println("Input 1 to Turn LED on and 2 to off");
 }
 
-int recv_command = 0;
 int power_state = 0;
 int hasNewCommand = 0;
+
 void loop() {
-  recv_command = 0;
+  
   if (IrReceiver.decode()) {
-    //IrReceiver.printIRResultShort(&Serial);
-    //recv_command = IrReceiver.decodedIRData.command;
     if (IrReceiver.decodedIRData.command > 0) {
       hasNewCommand = 1;
     }
-    //Serial.println(recv_command);
     flashLed();
     IrReceiver.resume();
   }
 
- 
-  if (hasNewCommand == 1) {
-    hasNewCommand = 0;
+  if (hasNewCommand == 1) {    
+    switch (IrReceiver.decodedIRData.command) {
+      case POWER_BUTTON:
+      {
+        if (power_state == 0) {
+          power_state = 1;
+          Serial.println("Power on");
+        } else if (power_state == 1) {
+          power_state = 0;
+          Serial.println("Power off");
+        }
+        break;
+      }
+      case VOLUME_UP:
+      {
+        Serial.println("Volume up");
+        break;
+      }
+      case VOLUME_DOWN:
+      {
+        Serial.println("Volume down");
+        break;
+      }
+      case REWIND:
+      {
+        Serial.println("Rewinding...");
+        break;
+      }
+      case FORWARD:
+      {
+        Serial.println("Forwarding...");
+        break;
+      }
+      default:
+        break;
+    }
     
-    if (IrReceiver.decodedIRData.command == 0x45 && power_state == 0) {
-      power_state = 1;
-      Serial.println("Power on");
-    } else if (IrReceiver.decodedIRData.command == 0x45 && power_state == 1) {
-      power_state = 0;
-      Serial.println("Power off");
-    }
-   
-    if (IrReceiver.decodedIRData.command == 0x46) {
-      Serial.println("Volume up");
-    }
+    hasNewCommand = 0;
   }
-
-  
-
-  
-
-  
-
-  // put your main code here, to run repeatedly:
-  // if (Serial.available())
-  // {
-  //   int state = Serial.parseInt();
-  //   if (state == 1)
-  //   {
-  //     digitalWrite(LED_PIN, HIGH);
-  //     Serial.println("Command completed LED turned ON");
-  //   }
-  //   if (state == 2)
-  //   {
-  //     digitalWrite(LED_PIN, LOW);
-  //     Serial.println("Command completed LED turned OFF");
-  //   }
-  // }
 }
